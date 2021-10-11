d996fe9568a7d5a779fbf780ea97dc420d4cde89
Nathaniel Schweinberg <314009+nathanielks@users.noreply.github.com>
2021-10-10 19:21:35 -0500

    add InventoryTable and InventoryManager components

---
+++ src/components/InventoryManager.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <AddToInventory />
+    <InventoryTable :items="inventory" />
+  </div>
+</template>
+<script>
+import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
+import AddToInventory from '@/components/AddToInventory.vue'
+import InventoryTable from '@/components/InventoryTable.vue'
+
+export default defineComponent({
+  components: {
+    AddToInventory,
+    InventoryTable
+  },
+  setup () {
+    const store = useStore()
+    const inventory = computed(() => store.state.inventory)
+
+    return {
+      inventory
+    }
+  }
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
+    <InventoryManager/>
     <recipes-table/>
   </div>
 </template>
 
 <script>
+import InventoryManager from '@/components/InventoryManager.vue'
 import RecipesTable from '@/components/RecipesTable.vue'
 
 export default {
   name: 'Home',
   components: {
+    InventoryManager,
     RecipesTable
   }
 }
